bb3c18f14ad9756673674a3a6f95aaf2c1876fc0
DiegoDD42 <diegodantas42@gmail.com>
2023-04-07 18:57:33 -0300

    Update main.js

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./home";
 import Login from "./login";
@@ -8,6 +8,7 @@ const Main = () => (
   <Routes>
     <Route exact path="/home" element={<Home/>}/>
     <Route exact path="/login" element={<Login/>}/>
+    <Route exact path="/" element={<Navigate to="/login" />}/>
   </Routes>
 )
 
